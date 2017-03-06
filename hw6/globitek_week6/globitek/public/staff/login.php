2422e2bd4dd758683d52eaa4f44f77e330be87d3
wl36901 <wl36901@gmail.com>
2017-03-06 01:57:15 -0800

    Signed-off-by: wl36901 <wl36901@gmail.com>

---
+++ hw6/globitek_week6/globitek/public/staff/login.php
@@ -0,0 +1,94 @@
+<?php
+require_once('../../private/initialize.php');
+
+// Until we learn about encryption, we will use an unencrypted
+// master password as a stand-in. It should go without saying
+// that this should *never* be done in real production code.
+//$master_password = 'secret';
+
+// Set default values for all variables the page needs.
+$errors = array();
+$username = '';
+$password = '';
+$remaining_locktime = 0;
+
+if(is_post_request() && request_is_same_domain()) {
+  ensure_csrf_token_valid();
+
+  // Confirm that values are present before accessing them.
+  if(isset($_POST['username'])) { $username = $_POST['username']; }
+  if(isset($_POST['password'])) { $password = h($_POST['password']); }
+
+  // Validations
+  if (is_blank($username)) {
+    $errors[] = "Username cannot be blank.";
+  }
+  if (is_blank($password)) {
+    $errors[] = "Password cannot be blank.";
+  }
+  
+  $remaining_locktime = throttle_time($username);
+  if($remaining_locktime === 0){
+  
+  // If there were no errors, submit data to database
+	if (empty($errors)) {
+
+		$users_result = find_users_by_username($username);
+		// No loop, only one result
+		$user = db_fetch_assoc($users_result);
+		if($user) {
+		//if(1 ===1 ){
+		if(password_verify($password, $user['hashed_password'])) {
+			// Username found, password matches
+			log_in_user($user);
+			
+			//log success, reset the account's login recorder
+			reset_failed_login($user['username']);
+			
+			// Redirect to the staff menu after login
+			redirect_to('index.php');
+		} else {
+			// Username found, but password does not match.
+			$errors[] = "Log in was unsuccessful.";
+		}
+		} else {
+		// No username found
+		$errors[] ="Log in was not successful.";
+		}
+	}
+  }
+}
+
+?>
+<?php $page_title = 'Log in'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+<div id="menu">
+  <ul>
+    <li><a href="../index.php">Public Site</a></li>
+  </ul>
+</div>
+
+<div id="main-content">
+  <h1>Log in</h1>
+
+  <?php echo display_errors($errors); ?>
+  <?php
+	if($remaining_locktime !== 0){
+        echo 'Too many failed logins <br/> ';
+		echo gmstrftime('%H:%M:%S',$remaining_locktime);
+		echo '<br/><br/>';
+	}?>
+	
+
+  <form action="login.php" method="post">
+    <?php echo csrf_token_tag(); ?>
+    Username:<br />
+    <input type="text" name="username" value="<?php echo h($username); ?>" /><br />
+    Password:<br />
+    <input type="password" name="password" value="" /><br />
+    <input type="submit" name="submit" value="Submit"  />
+  </form>
+
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
